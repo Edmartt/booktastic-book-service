6a3686744aa82bb746e88c7fea364970d3820a28
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-08-30 13:36:24 -0500

    chore: adds godotenv call

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"github.com/edmartt/bookstatic-book-service/internal/database"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
 	dbConnectObject := database.Postgres{}
 	getConn := dbConnectObject.GetConnection()
 
