968648b5348e53cd100119db48ea4d2ba6ef64fa
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-08-30 13:39:18 -0500

    feat: adds postgres connection and ping method

---
+++ internal/database/postgres.go
@@ -0,0 +1,50 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq" // sqlx uses it indirectly
+)
+
+type Postgres struct {
+	user     string
+	db       string
+	password string
+	host     string
+	port     string
+}
+
+// GetConnection connects to specific database
+func (p Postgres) GetConnection() *sqlx.DB {
+	p.user = os.Getenv("PG_USER")
+	p.db = os.Getenv("PG_DB")
+	p.password = os.Getenv("PG_PASSWORD")
+	p.host = os.Getenv("HOST")
+	p.port = os.Getenv("PORT")
+
+	/*intPort, err := strconv.Atoi(p.port)
+
+	if err != nil {
+		log.Fatal("ERROR CONVERTING: ", err)
+	}*/
+
+	db, err := sqlx.Connect("postgres", fmt.Sprintf("user=%s dbname=%s password=%s host=%s port=%s sslmode=disable", p.user, p.db, p.password, p.host, p.port))
+
+	if err != nil {
+		log.Fatal("CONNECTION ERROR: ", err)
+	}
+
+	return db
+}
+
+// PingDB test database connection
+func (p Postgres) PingDB(db *sqlx.DB) {
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to PG")
+}
