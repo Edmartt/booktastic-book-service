d0c9f0d02cd61831e28f80a2ae96e6f67de2fd4a
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-09-03 15:15:44 -0500

    feat: adds new models tags for db

--- internal/books/models/models.go
+++ internal/books/models/models.go
@@ -10,10 +10,12 @@ const (
 
 // Books model
 type Books struct {
-	ID     string `json:"id"`
-	ISBN   string `json:"isbn"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
-	Year   string `json:"year"`
-	Status string `json:"status"`
+	ID          string `json:"id" db:"id"`
+	ISBN        string `json:"isbn" db:"isbn"`
+	Title       string `json:"title" db:"title"`
+	Pages       string `json:"pages" db:"pages"`
+	CurrentPage string `json:"curren_page" db:"curren_page"`
+	Author      string `json:"author" db:"author"`
+	Year        string `json:"year" db:"year"`
+	Status      string `json:"status" db:"status"`
 }
