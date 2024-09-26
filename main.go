96b8297dd73722967e1787773a1de395abe2e18c
Edmartt <47486245+Edmartt@users.noreply.github.com>
2024-09-26 13:44:57 -0500

    update: changes http server call

--- main.go
+++ main.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/edmartt/bookstatic-book-service/internal/application"
+	"github.com/edmartt/bookstatic-book-service/internal/books/data"
 	"github.com/edmartt/bookstatic-book-service/internal/database"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-
-	if err != nil {
+	if err := godotenv.Load(); err != nil {
 		log.Fatalf("ERROR ENV LOAD: %v", err)
 	}
+
 	dbConnectObject := database.SQLite{}
 	getConn := dbConnectObject.GetConnection()
 
 	database.PingDB(getConn)
+	db := data.NewRepository(dbConnectObject)
+	handlerObject := application.NewHandler(db)
+	server := application.HTTPServer{
+		Handler: *handlerObject,
+	}
+
+	if err := server.RunServer(os.Getenv("HTTP_PORT")); err != nil {
+		panic(err)
+	}
 }
